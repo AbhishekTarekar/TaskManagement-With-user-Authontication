8af5589c00571e26b898e4100eb4a58a6b901be3
Abhishek Tarekar <abhishektarekar63@gmail.com>
2026-06-07 12:16:51 +0530

    added backed files
    added files manualy

---
+++ server.js
@@ -0,0 +1,18 @@
+require("./config/db");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
